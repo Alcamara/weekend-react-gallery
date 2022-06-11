383c18d0715898269c136b0b1195e34cc9e25c37
Al Camara <Alhousseinec@gmail.com>
2022-06-11 06:43:55 -0500

    update router put request to update likes in database

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -9,18 +9,36 @@ const pool = require('../modules/pool')
 router.put('/like/:id', (req, res) => {
     console.log(req.params);
     const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
+    // for(const galleryItem of galleryItems) {
+    //     if(galleryItem.id == galleryId) {
+    //         galleryItem.likes += 1;
+    //     }
+    // }
+
+    const updateLikeNum = `
+    UPDATE gallery
+    SET likes = likes + 1
+    WHERE id = $1;
+    `
+    const queryParam = [
+        galleryId,
+    ]
+
+    pool.query(updateLikeNum,queryParam)
+        .then(()=>{
+            res.sendStatus(200);
+        }).catch((err)=>{
+            console.log('Put request failed',err);
+        })
+
+    
 }); // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
     const query = `
-    SELECT * FROM "gallery";
+    SELECT * FROM "gallery"
+    ORDER BY likes DESC;
     `
 
     pool.query(query)
